4e7ed3837d0aa6525748ab0be92bb9b2013af8d2
baljinnyam.html@gmail.com <baljinnyam.html@gmail.com>
2023-04-14 00:45:37 +0800

    commit

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,13 @@
+import Card from "@/components/Card";
 import Footer from "@/components/Footer";
 
 
-export default function Home (){
-  return <><Footer/></>
+export default function Home ():any{
+  return 
+  <>
+    
+      {/* <Card/> */}
+      <Footer/>
+    
+  </>
 }
