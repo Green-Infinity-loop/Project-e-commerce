9b227e8e08386a0e02a8a0cb5b4c5a09619a7cd0
namuunbaigali <118256485+namuunbaigali@users.noreply.github.com>
2023-04-13 14:18:34 +0800

    as

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/Navbar";
 
-
-export default function Home (){
-  return <>Hello</>
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
