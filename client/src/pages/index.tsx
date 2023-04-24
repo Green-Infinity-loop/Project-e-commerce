c5a513dd1c732332de36800af2705aefc5b939c5
Baljinnyam07 <116425294+Baljinnyam07@users.noreply.github.com>
2023-04-24 17:31:39 +0800

    card

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,9 +10,9 @@ export default function Home(): any {
   return (
     <>
       <Navbar />
-      <HomePageTop />
+      {/* <HomePageTop /> */}
       <Card />
-      <Bottom />
+      {/* <Bottom /> */}
       <Footer />
     </>
   );
