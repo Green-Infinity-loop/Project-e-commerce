2021776dbcdf80a5b2aa979ebafbb5eccec972fd
namuunbaigali <118256485+namuunbaigali@users.noreply.github.com>
2023-04-24 19:01:46 +0800

    icon opuulsan , hover oopcilson shuu

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,11 +5,13 @@ import Footer from "@/components/Footer";
 import Bottom from "@/components/Bottom";
 
 import HomePageTop from "@/components/HomePageTop";
+import HomeCard from "@/components/HomeCard";
 
 export default function Home(): any {
   return (
     <>
       <Navbar />
+      <HomeCard />
       {/* <HomePageTop /> */}
       {/* <Card /> */}
       {/* <Bottom /> */}
