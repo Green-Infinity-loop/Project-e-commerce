e344c73fed0b8eedc3b37c388c8c7b2134cd152b
Baljinnyam <green.baljinnyam123@gmail.com>
2023-04-25 17:41:48 +0800

    card deer responsive newew

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -34,22 +34,95 @@ const products = [
     image: "http://via.placeholder.com/480x600",
     price: "599$",
   },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
+  {
+    name: "Apple Watch Series 7 GPS",
+    image: "http://via.placeholder.com/480x600",
+    price: "599$",
+  },
 ];
 
 export default function Card(): any {
   return (
     <div className="container">
-      <div className="grid grid-cols-4 gap-5 flex ">
+      <div className="grid xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 gap-4">
           {products.map((product) =>{
             return (
-              <div className="m-5 bg-white border text-center  rounded-lg h-[42vh] relative w-[100%]  shadow-xl">
-                <div className="absolute">
-                  <Image className="" alt="product image" src={product.image} width={300} height={200}/>
-                  <Link href="#">
-                      <h5 className="mb-2 text-[#214842] hover:text-[#17887A] p-8 pb-0 text-2xl tracking-tight text-black">{product.name}</h5>
-                  </Link>
-                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{product.price}</p>
-                  <Link className="text-black mt-1 p-2 border-2 border-[#17887A] rounded-xl hover:bg-[#17887A] hover:text-white" href='#'><button className="p-5">Add to Cart</button></Link>
+              <div className="m-4 bg-white border text-center center  rounded-lg row xl:h-[36vh] lg:h-[35vh] md:h-[40vh] sm:h-[50vh] cl:h-[100vh]  lg:w-[16vh]  relative   shadow-xl">
+                <div className="absolute w-[100%]">
+                  <Image className="rounded-lg" alt="product image" src={product.image} width={300} height={200} />
+                  <div className="xl:p-2 lg:p-2 md:p-3 sm:p-5 cl:p-5">
+                    <Link href="#">
+                      <h5 className=" text-[#214842] hover:text-[#17887A]   xl:text-xl tracking-tight text-black">{product.name}</h5>
+                    </Link>
+                    <p className=" font-normal text-gray-500 dark:text-gray-400">{product.price}</p>
+                    <Link className="text-black  border-2 border-[#17887A] p-2 rounded-2xl hover:bg-[#17887A] hover:text-white" href='#'><button className="p-5">Add to Cart</button></Link>
+                  </div>
                 </div>
           </div>
             )
