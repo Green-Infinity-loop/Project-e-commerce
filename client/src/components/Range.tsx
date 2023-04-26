e5b9d7b31cd83952cc62d8291898023ce6d685e7
namuunbaigali <118256485+namuunbaigali@users.noreply.github.com>
2023-04-26 16:55:17 +0800

    ddd

--- client/src/components/Range.tsx
+++ client/src/components/Range.tsx
@@ -58,34 +58,34 @@ export default function Range() {
               );
             })}
           </div>
-        <h1>Хайрцаг дахь шахмал</h1>
-        <div className="  ">
-          {filter.map((helber) => {
-            return (
-              <>
-                <div
-                  className="grid grid-cols-8 border p-3 my-4 rounded-xl "
-                  aria-labelledby="dropdownDefaultButton"
-                >
-                  <input
-                    id="default-checkbox"
-                    type="checkbox"
-                    value=""
-                    className=" col-start-1 col-end-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
+          <h1>Хайрцаг дахь шахмал</h1>
+          <div className="  ">
+            {filter.map((helber) => {
+              return (
+                <>
+                  <div
+                    className="grid grid-cols-8 border p-3 my-4 rounded-xl "
+                    aria-labelledby="dropdownDefaultButton"
+                  >
+                    <input
+                      id="default-checkbox"
+                      type="checkbox"
+                      value=""
+                      className="m-auto col-start-1 col-end-1 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
 
-                  <label className="text-left col-start-3 col-end-6 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300  ">
-                    {helber.title}
-                  </label>
-                  <p className="col-start-7 col-end-8 text-right">
-                    {helber.count}
-                  </p>
-                </div>
-              </>
-            );
-          })}
+                    <label className="text-left col-start-2 col-end-6 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300  ">
+                      {helber.title}
+                    </label>
+                    <p className="col-start-7 col-end-8 text-right">
+                      {helber.count}
+                    </p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
--- client/src/pages/searchPage.tsx
+++ client/src/pages/searchPage.tsx
@@ -1,27 +1,26 @@
 import Range from "@/components/Range";
 import SearchPageCard from "@/components/SearchPageCard";
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function searchPage() {
   return (
     <>
-      <Navbar/>
-    <div className="my-10">
-    <div className="container">
-      <div className="text-black">Search</div>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="text-black">
-          <Range />
-        </div>
-        <div className="col-span-2 text-black">
-          <SearchPageCard/>
+      <Navbar />
+      <div className="my-10">
+        <div className="container">
+          <div className="text-black">Search</div>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="text-black">
+              <Range />
+            </div>
+            <div className="col-span-2 text-black">
+              <SearchPageCard />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 }
