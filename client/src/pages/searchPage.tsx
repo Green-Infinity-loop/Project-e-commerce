63ba18c3fec55256e74b5ce142026fef1d742f16
namuunbaigali <118256485+namuunbaigali@users.noreply.github.com>
2023-04-26 16:43:02 +0800

    filter

--- client/src/pages/searchPage.tsx
+++ client/src/pages/searchPage.tsx
@@ -1,15 +1,15 @@
+import Range from "@/components/Range";
+
 export default function searchPage() {
   return (
     <div className="container">
-        <div className="text-black">Search</div>
-        <div className="grid grid-cols-3 gap-4">
+      <div className="text-black">Search</div>
+      <div className="grid grid-cols-3 gap-4">
         <div className="text-black">
-            hello
+          <Range />
         </div>
-        <div className="col-span-2 text-black">
-            hi
-        </div>
-    </div>    
+        <div className="col-span-2 text-black">hi</div>
+      </div>
     </div>
-  )
+  );
 }
