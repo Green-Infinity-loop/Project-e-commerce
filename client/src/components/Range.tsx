b50b1ef8db7f4971f7bdebd0901a90f69ab9e7f4
namuunbaigali <118256485+namuunbaigali@users.noreply.github.com>
2023-04-27 13:45:42 +0800

    и

--- client/src/components/Range.tsx
+++ client/src/components/Range.tsx
@@ -17,7 +17,7 @@ export default function Range() {
   return (
     <>
       <div className="">
-        <div>
+        <div className="mt-4">
           <input
             type="range"
             className="transparent h-1.5 w-full cursor-pointer appearance-none rounded-lg border-transparent bg-neutral-200"
@@ -35,11 +35,38 @@ export default function Range() {
 
         <div className=" mt-5">
           <h1>Хэлбэр</h1>
+
+          {filter.map((helber) => {
+            return (
+              <>
+                <div className="grid grid-cols-8 border p-3 my-4 rounded-xl ">
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    value=""
+                    className=" col-start-1 col-end-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+
+                  <label className="text-left col-start-3 col-end-6 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300  ">
+                    {helber.title}
+                  </label>
+                  <p className="col-start-7 col-end-8 text-right">
+                    {helber.count}
+                  </p>
+                </div>
+              </>
+            );
+          })}
+
+          <h1>Хайрцаг дахь шахмал</h1>
           <div className="  ">
             {filter.map((helber) => {
               return (
                 <>
-                  <div className="grid grid-cols-8 border p-3 my-4 rounded-xl">
+                  <div
+                    className="grid grid-cols-8 border p-3 my-4 rounded-xl "
+                    aria-labelledby="dropdownDefaultButton"
+                  >
                     <input
                       id="default-checkbox"
                       type="checkbox"
@@ -58,34 +85,7 @@ export default function Range() {
               );
             })}
           </div>
-        <h1>Хайрцаг дахь шахмал</h1>
-        <div className="  ">
-          {filter.map((helber) => {
-            return (
-              <>
-                <div
-                  className="grid grid-cols-8 border p-3 my-4 rounded-xl "
-                  aria-labelledby="dropdownDefaultButton"
-                >
-                  <input
-                    id="default-checkbox"
-                    type="checkbox"
-                    value=""
-                    className=" col-start-1 col-end-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-
-                  <label className="text-left col-start-3 col-end-6 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300  ">
-                    {helber.title}
-                  </label>
-                  <p className="col-start-7 col-end-8 text-right">
-                    {helber.count}
-                  </p>
-                </div>
-              </>
-            );
-          })}
         </div>
-      </div>
       </div>
     </>
   );
