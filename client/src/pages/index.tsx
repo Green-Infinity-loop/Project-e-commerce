e7d2395d42ad75b22891085aa792bd56ed305fd4
Baljinnyam <green.baljinnyam123@gmail.com>
2023-05-02 18:02:52 +0800

    limit

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,7 +3,7 @@ import {Card} from "@/components/Card";
 import Footer from "@/components/Footer";
 
 import Bottom from "@/components/Bottom";
-
+import { useQuery } from "@/Hooks/useQuery";
 import HomePageTop from "@/components/HomePageTop";
 import LogIn from "@/pages/login";
 
@@ -18,7 +18,7 @@ import Header from "@/components/Header";
 import SignUp from "@/pages/signup";
 import Sidebar from "@/components/Sidebar";
 import { useToast } from "@/Hooks/useToast";
-import { Alert, AlertTitle, IconButton, Select, Snackbar, Stack } from "@mui/material";
+import { Alert, AlertTitle, IconButton, Snackbar, Stack } from "@mui/material";
 import { GetServerSidePropsContext } from "next";
 import axios from "axios";
 import { IProduct } from "@/interfaces/product";
@@ -26,12 +26,13 @@ import { useRouter } from "next/router";
 import { nanoid } from "nanoid";
 import { CardSkelton } from "@/components/CardSkelton";
 import useLoader from "@/Hooks/useLoader";
+import { Select } from "@/components/Select";
 
 export async function getServerSideProps(context: GetServerSidePropsContext){
   const { query } = context;
   const {limit = 24 } = query;
   const response = await axios.get(
-    `http://localhost:8080/products?limit=${24}`
+    `http://localhost:8080/products/?limit=${limit}`
   );
   const { data } = await response;
   console.log(data)
@@ -50,6 +51,7 @@ export default function Home({data}:{data:any}): JSX.Element {
   const {query} = router;
   const loading = useLoader();
   const { limit = 24 } = query
+  const { addQuery } = useQuery();
 
   console.log("products:", products)
   return (
@@ -67,6 +69,22 @@ export default function Home({data}:{data:any}): JSX.Element {
       {/* <Button onClick={showModal}>modal</Button> */}
       {/* <HomeCard /> */}
       {/* <HomePageTop /> */}
+
+
+      <Select
+              items={[
+                { value: "6", label: "6" },
+                { value: "12", label: "12" },
+                { value: "24", label: "24" },
+                { value: "48", label: "48" },
+              ]}
+              onChange={(e) => {
+                addQuery({ limit: e.target.value });
+              }}
+              value={limit + ""}
+              itemValue={"value"}
+              itemLabel={"label"}
+            />
       
         <div className="container">
           <div className="grid xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 gap-4"> 
