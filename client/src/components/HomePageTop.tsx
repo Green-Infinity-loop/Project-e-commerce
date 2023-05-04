45ef58f5073a7b8a0b231a84e5fc3e0fdac2d22b
Baljinnyam <green.baljinnyam123@gmail.com>
2023-05-04 18:31:24 +0800

    finished

--- client/src/components/HomePageTop.tsx
+++ client/src/components/HomePageTop.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { text } from "stream/consumers";
-import Card from "./Card";
+// import Card from "./Card";
 import Dashed from "./Dashed";
+import pills from "../image/icon/icons8-pills-100.png";
+import business from "../image/icon/icons8-business-80.png";
+import bottle from "../image/icon/icons8-supplement-bottle-100.png"
+
 
 // export default function HomePageTop() {
 //   const datas = [
@@ -28,50 +33,53 @@ import Dashed from "./Dashed";
 export default function HomePageTop() {
   const datas = [
     {
-      imgUrl: "icon/darhlaaa.svg",
-      title: "Дархлаа",
+      imgUrl: pills,
+      title: "See Your Current Saving",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, itaque?",
+        "Create an account to transfer your existing prescription",
     },
     {
-      imgUrl: "icon/darhlaaa.svg",
-      title: "Эм бэлдмэл",
+      imgUrl: business,
+      title: "Send In a Prescription",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, itaque?",
+       "Create an account to transfer your existing prescription",
     },
     {
-      imgUrl: "icon/darhlaaa.svg",
-      title: "Эм бэлдмэл",
+      imgUrl: bottle,
+      title: "Receive Your Delivery",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, itaque?",
+        "Create an account to transfer your existing prescription",
     },
   ];
 
   return (
     <>
       <div className="   mx-auto  h-[80vh]  bg-[#615cbe] py-9">
-        <h1 className="my-8 font-bold text-4xl text-center text-white">
-          Эмийн төрөл
+        <h1 className="my-8 font-bold text-[80px] p-10  text-4xl text-center text-white ease-in-out">
+          Getting started is easy
         </h1>
-        <div className="grid grid-flow-col grid-cols-3 gap-4 container">
+        <div className="grid  grid-flow-col grid-cols-3 gap-4 container">
           {datas.map((data) => {
             return (
               <>
-                <div className="group relative">
-                  <div className=" h-96 group duration-500 flex flex-wrap bg-[#c4b5fd] border-t-4 border-[#d946ef] group-hover:bg-white group-hover:translate-y-0 translate-y-4 transform transition duration-300 ease-in-out">
-                    <img
+                <div className="group relative p-10">
+                  <div className=" h-96  group duration-500 flex flex-wrap bg-[#c4b5fd] group-hover:h-[35vh] border-t-4 border-[#d946ef] rounded-lg group-hover:bg-white group-hover:translate-y-0 translate-y-4 transform transition duration-1000 ease-in-out">
+                    {/* <img
                       src={data.imgUrl}
                       alt=" darhlaa bolon em "
                       className=" group-hover:scale-110 transition duration-300 ease-in-out  object-cover w-66 m-auto h-60 flex justify-center p-5"
-                    />
-                    <div className="text-white font-bold opacity-75 p-5  transform-gpu space-y-6  group-hover:text-slate-900">
+                    /> */}
+                   <div className="p-5 m-5 ">
+                      <Image src={data.imgUrl} width={"100%"} height={'100%'}></Image>
+                   </div>
+                    <div className="text-white p-10 font-bold opacity-75 p-5  transform-gpu space-y-6  group-hover:text-slate-900">
                       <h1 className="text-2xl"> {data.title}</h1>
                       <p className="text-white-600/25 text-start">
                         {data.description}
                       </p>
                     </div>
-                    <div className=" transition duration-300 ease-in-out cursor-pointer absolute to-transparent  inset-x-0 text-white ">
-                      <button className="text-black rounded-lg my-4 flex justify-center bg-[#d946ef] absolute hidden group-hover:block z-50 -bottom-50 ">
+                    <div className=" transition duration-300 ease-in-out cursor-pointer  to-transparent  inset-x-0 text-white ">
+                      <button className="text-black group hidden group-hover:block  rounded-lg my-4 p-3 px-4 m-10 flex justify-center bg-[#d946ef]  ">
                         Learn more
                       </button>
                     </div>
