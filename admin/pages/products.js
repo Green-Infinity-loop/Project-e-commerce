4d17f61f5488cce315198ecb321d09346e1d0ed4
Dulguun <Dulguunn0127@gmail.com>
2023-05-23 14:59:18 +0800

    Admin dre table uusgesen

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 export default function products() {
   const { items: products } = useCrud("products");
+  console.log(products);
   return (
     <Layout>
       <Link
@@ -16,8 +17,10 @@ export default function products() {
         <thead>
           <tr>
             <td>product Name</td>
+            <td>product ID</td>
             <td>Image</td>
             <td>Price</td>
+            <td>location</td>
           </tr>
         </thead>
 
@@ -25,9 +28,12 @@ export default function products() {
           {products.map((product) => (
             <tr>
               <td>{product.name}</td>
+              <td>{product._id}</td>
               <td>{product.image}</td>
               <td>{product.price}</td>
-              <td>
+              <td>{product.location.name}</td>
+              <td></td>
+              <div className="flex">
                 <Link href={"/products/" + product._id}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +51,24 @@ export default function products() {
                   </svg>
                   edit
                 </Link>
-              </td>
+                <Link href={"/products/" + product._id}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                    />
+                  </svg>
+                  delete
+                </Link>
+              </div>
             </tr>
           ))}
         </tbody>
